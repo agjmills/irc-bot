5c6fd1f3dff1886150e44f633bbc3555d91d6366
Alex Mills <alex@asdfx.us>
2019-01-21 14:20:51 +0000

    load modules by reflection

--- buttbot.py
+++ buttbot.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 # -*- coding: utf8 -*-
 import JustIRC
-import requests
 import re
 from pyshorteners import Shortener
 import time
 import yaml 
 import sys
+import pkgutil
+import Plugins
+import requests
+import importlib
 
 class Buttbot:
     configuration = {}
     bot = None
+    modules = []
 
     def __init__(self):
         self.configure()
@@ -18,11 +22,21 @@ class Buttbot:
 
     def setup_bot(self):
         self.bot =  JustIRC.IRCConnection()
+        self.import_plugins('Plugins')
         self.bot.on_connect.append(self.on_connect)
         self.bot.on_welcome.append(self.on_welcome)
         self.bot.on_public_message.append(self.on_message)
         self.bot.connect("irc.freenode.net")
-        self.bot.run_loop()
+        try:
+            self.bot.run_loop()
+        except Exception as exception:
+            self.bot.send_message("tinyhippo", "An error occurred, which has been logged.")
+            self.bot.send_message("tinyhippo", "{}".format(str(exception)))
+ 
+    def import_plugins(self, pkg_dir):
+        for (module_loader, name, ispkg) in pkgutil.iter_modules([pkg_dir]):
+            mod = importlib.import_module('.' + name, pkg_dir)
+            self.bot.on_public_message.append(mod.on_message)
     
     def configure(self):
         with open("conf.yml", 'r') as stream:
@@ -42,56 +56,33 @@ class Buttbot:
             bot.join_channel(channel)
             time.sleep(1)
 
-    def get_weather(self, message):
-        if len(message.split()) > 1:
-            weather_data = requests.get("http://api.apixu.com/v1/current.json",params={"key":self.configuration["weather_api_key"],"q":message.split(" ", 1)[1]}).json()
-            if not 'error' in weather_data:
-                weather_string = "\"{}\" and the temperature is {}°C ({}°F)".format(weather_data["current"]["condition"]["text"],weather_data["current"]["temp_c"], weather_data["current"]["temp_f"])
-                location_string = "{}, {}, {}".format(weather_data["location"]["name"],weather_data["location"]["region"],weather_data["location"]["country"])
-                return "The weather in {} is {}.".format(location_string, weather_string)
-            else:
-                return "{}".format(weather_data["error"]["message"])
-
-    def get_forecast(self, message):
-        if len(message.split()) > 1:
-            weather_data = requests.get("http://api.apixu.com/v1/forecast.json",params={"key":self.configuration["weather_api_key"],"q":message.split(" ", 1)[1]}).json()
-            if not 'error' in weather_data:
-                max_temperature = "{}°C ({}°F)".format(weather_data["forecast"]["forecastday"][0]["day"]["maxtemp_c"],weather_data["forecast"]["forecastday"][0]["day"]["maxtemp_f"])
-                min_temperature = "{}°C ({}°F)".format(weather_data["forecast"]["forecastday"][0]["day"]["mintemp_c"],weather_data["forecast"]["forecastday"][0]["day"]["mintemp_f"])
-                weather_string = "\"{}\" with a maximum temperature of {} and a minimum temperature of {}".format(weather_data["forecast"]["forecastday"][0]["day"]["condition"]["text"], max_temperature, min_temperature)
-                location_string = "{}, {}, {}".format(weather_data["location"]["name"],weather_data["location"]["region"],weather_data["location"]["country"])
-                return "The 24-hour forecast for {} is {}.".format(location_string, weather_string)
-            else:
-                return "{}".format(weather_data["error"]["message"])
-
-    def get_url_title(self, url):
-        html = requests.get(url).text
-        title_match = re.search("<title>(.*?)</title>", html)
-        try:
-            shortener = Shortener('Isgd')
-            short_url = shortener.short(url)
-        except:
-            short_url = None
+    def get_url_title(self, channel, url):
+        if url.endswith('mp3') is False:
+            data = requests.get(url)
+            title_match = re.search("<title>(.*?)</title>", html)
+            try:
+                shortener = Shortener('Isgd')
+                short_url = shortener.short(url)
+            except:
+                short_url = None
     
-        if title_match and short_url:
-            return "^ {} → {}".format(title_match.group(1), short_url)
-        if title_match and short_url is None:
-            return "^ {}".format(title_match.group(1))
-        return 'Could not retrieve the title of the URL'
+            if title_match and short_url:
+                bot.send_message(channel, "^ {} → {}".format(title_match.group(1), short_url))
+                return
+            if title_match and short_url is None:
+                bot.send_message(channel, "^ {}".format(title_match.group(1)))
+                return
+            bot.send_message(channel, 'Could not retrieve the title of the URL')
 
     def on_message(self, bot, channel, sender, message):
         try:
             if len(message.split()) == 0:
                 message = "." #For people who try to crash bots with just spaces
-            if message.split()[0] == ".weather":
-                bot.send_message(channel, self.get_weather(message))
-            if message.split()[0] == ".forecast":
-                bot.send_message(channel, self.get_forecast(message))
             if message.split()[0] == ".source":
                 bot.send_message(channel, "My source code is here: https://github.com/buttbot-irc/buttbot")
             for message_part in message.split():
                 if message_part.startswith("http://") or message_part.startswith("https://"):
-                    bot.send_message(channel, self.get_url_title(message_part))
+                    self.get_url_title(channel, message_part)
         except Exception as exception:
             bot.send_message(channel, "An error occurred, which has been logged.")
             bot.send_message("tinyhippo", "The following message:")
